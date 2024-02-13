bec6fceb09f75fb984d96024e4b5bfe61f590647
shambhurajep71 <shambhurajepatil71@gmail.com>
2024-02-13 21:15:38 +0530

    Dynamic ticket creation

--- Kanban-board/kanban.js
+++ Kanban-board/kanban.js
@@ -1,6 +1,8 @@
 const addBtn = document.querySelector(".add-btn");
 const modelCont = document.querySelector(".model-cont");
-
+const mainTicketCont = document.querySelector(".main-ticket-cont");
+const createTicketBtn =document.querySelector(".create-btn");
+const textArea = document.querySelector(".textarea-cont");
 let addTicketFlag= false;
 addBtn.addEventListener("click", () => {
     addTicketFlag=!addTicketFlag;
@@ -10,3 +12,23 @@ addBtn.addEventListener("click", () => {
         modelCont.style.display="none";
     }
 });
+
+// crating ticket Dynamically
+createTicketBtn.addEventListener("click", (event) => {
+    createTicket(textArea.value);
+    modelCont.style.display="none";
+    textArea.value="";
+});
+
+const createTicket =(ticketInfo) =>{
+   let ticketCont= document.createElement("div");
+    ticketCont.setAttribute("class", "ticket-cont");
+   ticketCont.innerHTML=` <div class="ticket-color"> </div>
+   <div class="ticket-id">${shortid()/**? crating a random id */}</div>
+   <div class="task-area">${ticketInfo}</div>
+   <div class="lock-btn"><i class="fa-solid fa-lock"></i></div>`
+   mainTicketCont.appendChild(ticketCont);
+}
+
+
+
