23d2cd716b73d7794847e0afef7645c553d286ac
shambhurajep71 <shambhurajepatil71@gmail.com>
2024-02-24 17:13:23 +0530

    added functionality for local storage

--- Kanban-board/kanban.js
+++ Kanban-board/kanban.js
@@ -10,7 +10,125 @@ let ticketStatusColor ="black"
 let removeTicketFlag= false;
 
  let ticketArr=[];
- console.log(ticketArr);
+ // we need to get the ticket from local storage after the refersh
+ 
+
+ // create ticket
+const createTicket =(ticketStatusColor,ticketInfo,ticketUniqueId) =>{
+    const ticketId= ticketUniqueId || shortid();
+  const ticketCont= document.createElement("div");
+    ticketCont.setAttribute("class", "ticket-cont");
+   ticketCont.innerHTML=
+   ` <div class="${ ticketStatusColor} ticket-color">
+    </div>
+   <div class="ticket-id">${ticketId/**? crating a random id */}
+   </div>
+   <div class="task-area">
+   ${ticketInfo}</div>
+   <div class="lock-btn">
+   <i class="fa-solid fa-lock"></i>
+   </div>`
+   mainTicketCont.appendChild(ticketCont);
+   if(ticketUniqueId.length<=0){
+    ticketArr.push({ticketId,ticketInfo,ticketStatusColor});
+   }
+   // saving the created ticket to local storeage
+   localStorage.setItem("tasks",JSON.stringify(ticketArr));
+
+   removeThisTicket(ticketCont,ticketId);
+   handleEdit(ticketCont,ticketId);
+   updateStatusColor(ticketCont,ticketId);
+}
+// remove functionality
+// activating the delete button:
+
+removeBtn.addEventListener("click", ()=>{
+    removeTicketFlag=!removeTicketFlag;
+    if(removeTicketFlag===true){
+        window.alert("Delete Button is activeted");
+        removeBtn.style.color="red";
+    }else{
+        window.alert("Delete Button is Deactivated");
+        removeBtn.style.color="white";
+    }
+    
+})
+
+// removing the ticket after clicking on a particular ticket
+
+const removeThisTicket = (ticketCont, ticketId)=> {
+    ticketCont.addEventListener("click", () =>{
+        if(removeTicketFlag===true){
+            ticketCont.remove();
+            const ticketgettingDeleted = getTicket(ticketId);
+            ticketArr.splice(ticketgettingDeleted,1);
+            //delting the ticket from  the local storage which user deleted
+            localStorage.setItem("tasks",JSON.stringify(ticketArr));
+            console.log("after deleting the ticket"+ ticketArr);
+        }
+   });
+}
+//Edit functionality
+
+const handleEdit = (ticket,ticketId)=> {
+    const ticketLockElem = ticket.querySelector(".lock-btn");
+    const lockBtnIcon = ticketLockElem.children[0];
+    const editableTextArea = ticket.querySelector(".task-area");
+
+    lockBtnIcon.addEventListener("click", () =>{
+        if(lockBtnIcon.classList.contains("fa-lock")){
+            lockBtnIcon.classList.remove("fa-lock");
+            lockBtnIcon.classList.add("fa-lock-open");
+            editableTextArea.setAttribute("contenteditable", "true");
+        }else{
+            lockBtnIcon.classList.remove("fa-lock-open");
+            lockBtnIcon.classList.add("fa-lock");
+            editableTextArea.setAttribute("contenteditable", "false");
+            // saving the edited content
+            const ticketTobeUpdted = ticketArr.findIndex(
+                 (ticketInArr) => ticketInArr.ticketId === ticketId
+            );
+            ticketArr[ticketTobeUpdted].ticketInfo = editableTextArea.innerText;
+            // saving the edited data in the local storage fro the particular ticket
+            localStorage.setItem("tasks",JSON.stringify(ticketArr));
+        }
+    });
+};
+
+// updating stauus color of created ticket
+ let colors= ["lightpink", "lightgreen", "lightblue", "black"];
+
+const updateStatusColor = (ticket,ticketId) => {
+    const colorBand  = ticket.querySelector(".ticket-color");
+    colorBand.addEventListener("click", (event) => {
+        let currentColor = "";
+         for( let idx=0; idx<colorBand.classList.length; idx++){
+             if(colorBand.classList[idx] !== "ticket-color"){
+            currentColor=colorBand.classList[idx];
+            }
+         }
+         let currentColorIndex= colors.indexOf(currentColor);
+         let nextColor =colors[(currentColorIndex+1)%colors.length];
+            colorBand.classList.remove(currentColor);
+            colorBand.classList.add(nextColor);
+            //saving the status color when user decide to change status original to next color
+            const ticketTobeUpdted = ticketArr.findIndex(
+                (ticketInArr) => ticketInArr.ticketId === ticketId
+            );
+           ticketArr[ticketTobeUpdted].ticketStatusColor = nextColor;
+           // saving the updated status color in the local storage  
+           localStorage.setItem("tasks",JSON.stringify(ticketArr));
+    })
+
+}
+ if(localStorage.getItem("tasks")){
+    ticketArr=JSON.parse(localStorage.getItem("tasks"));
+    ticketArr.forEach((ticket) => {
+    createTicket(ticket.ticketStatusColor,ticket.ticketInfo,ticket.ticketId);
+    })
+    console.log(ticketArr);
+ }
+  
 // clearing the currently selected status
  const cleanUpSelectedStatus = () =>{
     allStatusColor.forEach((currentEle) => {
@@ -47,106 +165,6 @@ allStatusColor.forEach((statusColor) => {
         statusColor.classList.add("active-ticket-color");
     })
 });
-
-const createTicket =(ticketStatusColor,ticketInfo,ticketUniqueId) =>{
-    const ticketId= ticketUniqueId || shortid();
-  const ticketCont= document.createElement("div");
-    ticketCont.setAttribute("class", "ticket-cont");
-   ticketCont.innerHTML=
-   ` <div class="${ ticketStatusColor} ticket-color">
-    </div>
-   <div class="ticket-id">${ticketId/**? crating a random id */}
-   </div>
-   <div class="task-area">
-   ${ticketInfo}</div>
-   <div class="lock-btn">
-   <i class="fa-solid fa-lock"></i>
-   </div>`
-   mainTicketCont.appendChild(ticketCont);
-   if(ticketUniqueId.length<=0){
-    ticketArr.push({ticketId,ticketInfo,ticketStatusColor});
-   }
-   removeThisTicket(ticketCont,ticketId);
-   handleEdit(ticketCont,ticketId);
-   updateStatusColor(ticketCont,ticketId);
-}
-
-// remove functionality
-// activating the delete button:
-
-removeBtn.addEventListener("click", ()=>{
-    removeTicketFlag=!removeTicketFlag;
-    if(removeTicketFlag===true){
-        window.alert("Delete Button is activeted");
-        removeBtn.style.color="red";
-    }else{
-        window.alert("Delete Button is Deactivated");
-        removeBtn.style.color="white";
-    }
-    
-})
-
-// removing the ticket after clicking on a particular ticket
-
-const removeThisTicket = (ticketCont, ticketId)=> {
-    ticketCont.addEventListener("click", () =>{
-        if(removeTicketFlag===true){
-            ticketCont.remove();
-            const ticketgettingDeleted = getTicket(ticketId);
-            ticketArr.splice(ticketgettingDeleted,1);
-            console.log("after deleting the ticket"+ ticketArr);
-        }
-   });
-}
-//Edit functionality
-
-const handleEdit = (ticket,ticketId)=> {
-    const ticketLockElem = ticket.querySelector(".lock-btn");
-    const lockBtnIcon = ticketLockElem.children[0];
-    const editableTextArea = ticket.querySelector(".task-area");
-
-    lockBtnIcon.addEventListener("click", () =>{
-        if(lockBtnIcon.classList.contains("fa-lock")){
-            lockBtnIcon.classList.remove("fa-lock");
-            lockBtnIcon.classList.add("fa-lock-open");
-            editableTextArea.setAttribute("contenteditable", "true");
-        }else{
-            lockBtnIcon.classList.remove("fa-lock-open");
-            lockBtnIcon.classList.add("fa-lock");
-            editableTextArea.setAttribute("contenteditable", "false");
-            // saving the edited content
-            const ticketTobeUpdted = ticketArr.findIndex(
-                 (ticketInArr) => ticketInArr.ticketId === ticketId
-            );
-            ticketArr[ticketTobeUpdted].ticketInfo = editableTextArea.innerText;
-        }
-    });
-};
-
-// updating stauus color of created ticket
- let colors= ["lightpink", "lightgreen", "lightblue", "black"];
-
-const updateStatusColor = (ticket,ticketId) => {
-    const colorBand  = ticket.querySelector(".ticket-color");
-    colorBand.addEventListener("click", (event) => {
-        let currentColor = "";
-         for( let idx=0; idx<colorBand.classList.length; idx++){
-             if(colorBand.classList[idx] !== "ticket-color"){
-            currentColor=colorBand.classList[idx];
-            }
-         }
-         let currentColorIndex= colors.indexOf(currentColor);
-         let nextColor =colors[(currentColorIndex+1)%colors.length];
-            colorBand.classList.remove(currentColor);
-            colorBand.classList.add(nextColor);
-            //saving the status color when user decide to change status original to next color
-            const ticketTobeUpdted = ticketArr.findIndex(
-                (ticketInArr) => ticketInArr.ticketId === ticketId
-            );
-           ticketArr[ticketTobeUpdted].ticketStatusColor = nextColor;
-    })
-
-}
 
 // filter functionalty
 
